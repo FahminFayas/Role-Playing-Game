e30c1d887f86d11dcde373ef3974f5e61a75d843
FahminFayas <akfahmin@gmail.com>
2024-01-21 15:01:35 +0530

    Added variables for game state, buttons, and inventory. Implemented functions for town, store, cave, and buying health/weapons. Updated button actions and text content dynamically. Integrated weapon inventory and added functionality for selling weapons.

--- script.js
+++ script.js
@@ -15,6 +15,29 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [{name: "stick",power: 5},
+                {name: "dagger", power: 30},
+                {name: "claw hammer",power: 50},
+                {name: "sword",power: 100}];
+                const locations = [{
+                    name: "town square",
+                    "button text": ["Go to store","Go to cave","Fight dragon"],
+                    "button functions": [goStore,goCave,fightingDragon],
+                    text: "You are in the town square. You see a sign that says \"Store\"."
+                },
+                {
+                    name: "store",
+                    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+                    "button functions": [buyHealth, buyWeapon, goTown],
+                    text: "You enter the store."
+                  },
+                  {
+                    name: "cave",
+                    "button text": ["Fight slime","Fight fanged beast","Go to town square"],
+                    "button functions": [fightSlime,fightBeast,goTown],
+                    text: "You enter the cave. You see some monsters."
+                  
+                  }];
 
 //initialize buttons
 
@@ -23,33 +46,25 @@ button2.onclick = goCave;
 button3.onclick = fightingDragon;
 
 function update(location){
-    
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+  text.innerText = location.text;
 }
 
 function goTown(){
-    button1.innerText = "Go to store";
-  button2.innerText = "Go to cave";
-  button3.innerText = "Fight dragon";
-  button1.onclick = goStore;
-  button2.onclick = goCave;
-  button3.onclick = fightDragon;
-  text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+    update(locations[0]);
 }
 
 function goStore(){
-    button1.innerText = "Buy 10 health (10 gold)";
-    button2.innerText = "Buy weapon (30 gold)";
-    button3.innerText = "Go to townsquare";
-
-    button1.onclick = buyHealth;
-    button2.onclick = buyWeapon;
-    button3.onclick = goTown;
-
-    text.innerText = "You enter the store.";
+   update(locations[1])
 }
 
 function goCave(){
-    console.log("Going to cave.");
+    update(locations[2]);
 }
 
 function fightingDragon(){
@@ -57,9 +72,53 @@ function fightingDragon(){
 }
 
 function buyHealth(){
-
+    if(gold >= 10){
+        gold -= 10;
+        goldText.innerText = gold;
+        health += 10;
+        healthText.innerText = health;
+    }else{
+        text.innerText = "You do not have enough gold to buy health."
+    }
+    
 }
 
 function buyWeapon(){
+    if(currentWeapon<weapons.length - 1){
+        if(gold >= 30){
+        gold -= 30;
+        currentWeapon++;
+        goldText.innerText = gold;
+        let newWeapon =  weapons[currentWeapon].name;
+        text.innerText = "You now have a n" + newWeapon + ".";
+        inventory.push(newWeapon);
+        text.innerText += " In your inventory you have: " + inventory;
+        }else{
+        text.innerText = "You do not have enough gold to buy a weapon.";
+        }
+
+    }else{
+        text.innerText = "You already have the most powerful weapon!";
+        button2.innerText = "Sell weapon for 15 gold";
+        button2.onclick = sellWeapon;
+    }
+}
+
+function sellWeapon(){
+    if(inventory.length>1){
+        gold += 15;
+        goldText.innerText = gold;
+        let currentWeapon = inventory.shift();
+        text.innerText = "You sold a " + currentWeapon + ".";
+        text.innerText += " In your inventory you have: " + inventory;
+    }else{
+        text.innerText = "Don't sell your only weapon";
+    }
+}
+function fightSlime(){
+
+}
+
+function fightBeast(){
 
 }
